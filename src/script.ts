403a86dbe181cb77dcba604c1f45b9c84d669b62
Karolis J. <264249313+karolistech@users.noreply.github.com>
2026-05-16 05:26:19 +0300

    feat: add color input handling

--- src/script.ts
+++ src/script.ts
@@ -1,6 +1,10 @@
 const canvas = document.querySelector<HTMLDivElement>(".canvas")!;
+const penColorInput = document.querySelector<HTMLInputElement>("#pen-color")!;
+const canvasColorInput = document.querySelector<HTMLInputElement>("#canvas-color")!;
 
 const state = {
+  penColor: penColorInput.value,
+  canvasColor: canvasColorInput.value,
   gridSize: 16,
 };
 
@@ -17,8 +21,23 @@ function createGrid(size: number) {
   }
 }
 
+function updatePenColor() {
+  state.penColor = penColorInput.value;
+}
+
+function updateCanvasColor() {
+  state.canvasColor = canvasColorInput.value;
+  canvas.style.backgroundColor = state.canvasColor;
+}
+
+function setupEvents() {
+  penColorInput.addEventListener("input", updatePenColor);
+  canvasColorInput.addEventListener("input", updateCanvasColor);
+}
+
 function init() {
   createGrid(state.gridSize);
+  setupEvents();
 }
 
 init();
